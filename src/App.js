de81eac522ad0da8146db4e8348c0a2317579322
Melanie Rodas <mrodasl@uni.pe>
2022-10-25 17:24:18 -0500

    added font and Logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Logo from "./components/Logo";
 
 function App() {
-  return <div className="App">Hello World 🙂 !!!</div>;
+  return (
+    <div className="App">
+      <Logo />
+
+      <div>Hello World 🙂 !!!</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <h1>
+        <span>&lt;</span>Melanie-Rodas <span>/&gt;</span>
+      </h1>
+    </div>
+  );
+}

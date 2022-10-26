fb440341affab9caf014e8db82800188c8c4d202
Melanie Rodas <mrodasl@uni.pe>
2022-10-25 21:42:50 -0500

    navbar - added navbar shadow effect on scroll

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -13,8 +13,25 @@ export default function Navbar() {
     "Education",
     "Contact",
   ];
+  const [shadow, setShadow] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.pageYOffset > 80) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    // clean up code
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  //console.log(shadow);
   return (
-    <div className="navbar">
+    <div className={shadow ? "navbar active" : "navbar"}>
       <Logo />
       <ul className="navbar__item">
         {menuItems.map((item, i) => {

c43c3be594732546c7c225a0cee2cc609cf74987
Melanie Rodas <mrodasl@uni.pe>
2022-11-17 19:27:36 -0500

    feature_footer-section:added menuItems in the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./style.css";
 import Logo from "../Logo";
+import MenuItem from "../MenuItem";
 
 const Footer = () => {
+  const menuItems = [
+    "Home",
+    "About",
+    "Projects",
+    "Skills",
+    "Education",
+    "Contact",
+  ];
   return (
     <div className="footer">
-      <div className="footer__menu"></div>
+      <div className="footer__menu">
+        {menuItems.map((item, i) => {
+          return <MenuItem key={i} item={item} fontColor={"font-white"} />;
+        })}
+      </div>
       <div className="footer__logo">
         <Logo color="logo--colorB" />
       </div>
